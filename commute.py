b4da7df4d1a2062a68c0381149eedcf5c91ba11e
KhoiBui <khoibui94@gmail.com>
2017-08-13 20:30:53 -0700

    minor touchups

--- commute.py
+++ commute.py
@@ -1,10 +1,12 @@
-import googlemaps, requests, json, keys
-from twilio.rest import Client
-from datetime import datetime
+import requests, json, keys
+from googlemaps import Client as gmapsClient
+from twilio.rest import Client as twilioClient
+
+gclient = gmapsClient(keys.api_key)
 
 
 def send_update(message):
-    twilio_client = Client(keys.account_sid, keys.auth_token)
+    twilio_client = twilioClient(keys.account_sid, keys.auth_token)
     twilio_client.api.messages.create(to=keys.to_number,
                                       from_=keys.from_number,
                                       body=message)
@@ -16,15 +18,18 @@ def init_map():
                        destination=keys.destination,
                        alternatives='true',
                        departure_time='now',
+                       mode='driving',
                        key=keys.api_key)
 
     results = requests.get(url, maps_params)
     data = json.loads(results.content.decode('utf-8'))
-    legs_info = ''
+    legs_info = '\n'
 
     for i in range(len(data['routes'])):
         info = data['routes'][i]['summary'] + ': ' + str(data['routes'][i]['legs'][0]['duration']['text']) + '\n'
         legs_info += info
+        # debug
+        # print(data['routes'][i]['legs'])
 
     # debug
     print(legs_info)
@@ -34,7 +39,7 @@ def init_map():
 
 def main():
     maps_results = init_map()
-    #send_update(maps_results)
+    send_update(maps_results)
 
 if __name__ == '__main__':
     main()
